43827e903975854ae733ce9d9fb2d431e28f8cd3
Gabriel Räätäri Nyström <g7003062@gmail.com>
2026-01-30 10:08:03 +0100

    feat: add minimal application code

---
+++ src/main/java/ci/App.java
@@ -0,0 +1,11 @@
+package ci;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
+}
